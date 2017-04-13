c4616328cf52971aa07b63938b4029b632787032
Greg Hedin <greghedin@gmail.com>
2017-04-13 11:39:42 -0700

    test for 0 results

--- features/step_definitions/hooks.js
+++ features/step_definitions/hooks.js
@@ -2,26 +2,36 @@ const assert = require('assert')
 // const chalk = require('chalk')
 // const child_process = require('child_process')
 const { defineSupportCode } = require('cucumber')
-const readdirFilter = require('../../index').default
+// const readdirFilter = require('../../index').default
 
 defineSupportCode(function ({ After, Given, Then }) {
   Given('all the {stringInDoubleQuotes} in tests that start with a {stringInDoubleQuotes}',
     function (fsoType, expr, callback) {
       this.fso_type = fsoType
       this.starts_with_expr = expr
-
-      readdirFilter('tests/', this.starts_with_filter)
-        .then((filesArr) => {
-          this.objects = filesArr
-          callback()
-        }, (ex) => { throw ex })
+      this.filter_objects(this.starts_with_filter, callback)
+      // readdirFilter('tests/', this.starts_with_filter)
+      //   .then((filesArr) => {
+      //     this.objects = filesArr
+      //     callback()
+      //   }, (ex) => { throw ex })
     })
 
   Given('all the {stringInDoubleQuotes} in tests that end with {stringInDoubleQuotes}',
     function (fsoType, expr, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      callback(null, 'pending')
+      this.fso_type = fsoType
+      this.ends_with_expr = expr
+      this.filter_objects(this.ends_with_filter, callback)
     })
+  //   readdirFilter('tests/', this.ends_with_filter)
+  //     .then((filesArr) => {
+  //       this.objects = filesArr
+  //       callback()
+  //     }, (ex) => { throw ex })
+  // })
+  // Write code here that turns the phrase above into concrete actions
+  //   callback(null, 'pending')
+  // })
 
   Then('there should be \'{int}\' objects',
     function (fsoCount, callback) {
--- features/support/world.js
+++ features/support/world.js
@@ -1,14 +1,17 @@
 var { defineSupportCode } = require('cucumber')
+const readdirFilter = require('../../index').default
 // const assert = require('assert')
 
 function CustomWorld () {
+  this.objects = []
+
   this.starts_with_expressions = {
     alpha: /^[a-z]/i,
     number: /^[0-9]/
   }
 
   this.ends_with_expressions = {
-    txt: /\.txt$/i
+    '.txt': /\.txt$/i
   }
 
   this.fso_types = {
@@ -44,9 +47,10 @@ function CustomWorld () {
   this.starts_with_filter = (fsoName, stats) => {
     this.validate_expression(this.starts_with_expressions, this.starts_with_expr)
     this.validate_fso_type()
-    return fsoName.match(this.starts_with_expressions[this.starts_with_expr]) && stats[this.fso_types[this.fso_type]]()
-  }
 
+    return fsoName.match(this.starts_with_expressions[this.starts_with_expr]) &&
+      stats[this.fso_types[this.fso_type]]()
+  }
 
   this.ends_with_filter = (fsoName, stats) => {
     this.validate_expression(this.ends_with_expressions, this.ends_with_expr)
@@ -54,7 +58,13 @@ function CustomWorld () {
     return fsoName.match(this.starts_with_expressions[this.starts_with_expr]) && stats[this.fso_types[this.fso_type]]()
   }
 
-  this.objects = []
+  this.filter_objects = (filter, callback) => {
+    readdirFilter('tests/', filter)
+      .then((filesArr) => {
+        this.objects = filesArr
+        callback()
+      }, (ex) => { throw ex })
+  }
 }
 
 defineSupportCode(function ({ setWorldConstructor }) {
